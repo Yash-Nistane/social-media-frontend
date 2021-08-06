ce88b89384303e8d5e2f0427e5527c87f57320dc
Yash_Nistane <yashnistane406@gmail.com>
2021-08-06 13:09:00 +0530

    api changed

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const baseUrl = 'http://localhost:8800';
+// export const baseUrl = 'http://localhost:8800';
+
+export const baseUrl = 'https://social-media-rest-server.herokuapp.com';
 
 export const api = `${baseUrl}/api`;
 
